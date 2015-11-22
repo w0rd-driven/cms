38215a67aff90335fa7ca213f69017fc83ce471a
Jeremy Brayton <jeremy@braytonium.com>
2015-11-22 16:40:43 -0500

    Restructured to keep bower task, using mainBowerFiles, next to the html task. Renamed src/dest to source/destination respectively. I'm not a fan of abbreviations that, while common, are annoying to look at.

--- gulpfile.js
+++ gulpfile.js
@@ -16,143 +16,149 @@ var gulp           = require("gulp"),
 
 var config = {
   paths: {
+    bower: {
+      source: "bower_components",
+      destination: "build/lib"
+    },
     html: {
-      src:  "app/**/*.html",
-      dest: "build"
+      source:  "app/**/*.html",
+      destination: "build"
     },
     javascript: {
-      src:  ["app/js/**/*.js"],
-      dest: "build/js"
+      source:  ["app/js/**/*.js"],
+      destination: "build/js"
     },
     css: {
-      src: ["app/css/**/*.css"],
-      dest: "build/css"
+      source: ["app/css/**/*.css"],
+      destination: "build/css"
     },
     images: {
-      src: ["app/images/**/*.jpg", "app/images/**/*.jpeg", "app/images/**/*.png"],
-      dest: "build/images"
+      source: ["app/images/**/*.jpg", "app/images/**/*.jpeg", "app/images/**/*.png"],
+      destination: "build/images"
     },
     less: {
-      src: ["app/less/**/*.less", "!app/less/includes/**"],
-      dest: "build/css"
+      source: ["app/less/**/*.less", "!app/less/includes/**"],
+      destination: "build/css"
     },
     sass: {
-      src: ["app/sass/**/*.scss", "!app/sass/includes/**"],
-      dest: "build/css"
-    },
-    bower: {
-      src: "bower_components",
-      dest: "build/lib"
+      source: ["app/sass/**/*.scss", "!app/sass/includes/**"],
+      destination: "build/css"
     },
     verbatim: {
-      src: ["app/manifest.json", "app/favicon.png"],
-      dest: "build"
+      source: ["app/manifest.json", "app/favicon.png"],
+      destination: "build"
     }
   }
 };
 
-gulp.task("html", function(){
-  return gulp.src(config.paths.html.src)
-    .pipe(inject(
-      gulp.src(
-        mainBowerFiles({ includeDev: true }),
-        {read: false, cwd: "bower_components"}
-      ),
-      {name: "bower", addPrefix: "lib"}
-    ))
-    .pipe(minifyHTML())
-    .pipe(gulp.dest(config.paths.html.dest));
+gulp.task("bower", function(){
+  return gulp.src(mainBowerFiles({ includeDev: true }), { base: config.paths.bower.source })
+    .pipe(gulp.dest(config.paths.bower.destination));
 });
 
-gulp.task("scripts", function(){
-  return gulp.src(config.paths.javascript.src)
+gulp.task("html", function(){
+  return gulp.src(config.paths.html.source)
+    .pipe(inject(gulp.src(mainBowerFiles({ includeDev: true }), { read: false, cwd: config.paths.bower.source }), {
+      name: "bower",
+      addPrefix: "lib"
+    }))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(config.paths.html.destination));
+});
+
+gulp.task("javascript", function(){
+  return gulp.src(config.paths.javascript.source)
     .pipe(sourcemaps.init())
     .pipe(concat("app.min.js"))
     .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.paths.javascript.dest));
+    .pipe(gulp.dest(config.paths.javascript.destination));
 });
 
 gulp.task("css", function(){
-  return gulp.src(config.paths.css.src)
+  return gulp.src(config.paths.css.source)
     .pipe(sourcemaps.init())
     .pipe(cssmin())
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(config.paths.css.dest))
+    .pipe(gulp.dest(config.paths.css.destination))
     .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task("images", function(){
-  return gulp.src(config.paths.images.src)
+  return gulp.src(config.paths.images.source)
     .pipe(imagemin({
       progressive: true,
       interlaced: true
     }))
-    .pipe(gulp.dest(config.paths.images.dest));
-});
-
-gulp.task("bower", function(){
-  return gulp.src(mainBowerFiles({ includeDev: true }), {base: "bower_components"})
-    .pipe(gulp.dest(config.paths.bower.dest));
+    .pipe(gulp.dest(config.paths.images.destination));
 });
 
 gulp.task("less", function(){
-  return gulp.src(config.paths.less.src)
+  return gulp.src(config.paths.less.source)
     .pipe(sourcemaps.init())
     .pipe(less({
       paths: ["bower_components/bootstrap/less"]
     }))
     .pipe(uncss({
-      html: glob.sync(config.paths.html.src),
+      html: glob.sync(config.paths.html.source),
     }))
     .pipe(concat("main.min.css"))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(config.paths.css.dest))
+    .pipe(gulp.dest(config.paths.css.destination))
     .pipe(filter("**/*.css"))
     .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task("sass", function(){
-  return gulp.src(config.paths.sass.src)
+  return gulp.src(config.paths.sass.source)
     .pipe(sourcemaps.init())
     .pipe(sass({
       outputStyle: "compressed",
-      includePaths: [ config.paths.bower.src + "/bootstrap-sass/assets/stylesheets", config.paths.bower.src + "/font-awesome/scss" ],
+      includePaths: [ config.paths.bower.source + "/bootstrap-sass/assets/stylesheets", config.paths.bower.source + "/font-awesome/scss" ],
       sourceComments: false
     }).on("error", sass.logError))
     .pipe(uncss({
-      html: glob.sync(config.paths.html.src),
+      html: glob.sync(config.paths.html.source),
     }))
     .pipe(concat("main.min.css"))
     .pipe(sourcemaps.write("."))
-    .pipe(gulp.dest(config.paths.css.dest))
+    .pipe(gulp.dest(config.paths.css.destination))
     .pipe(filter("**/*.css"))
     .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task("verbatim", function(){
-  gulp.src(config.paths.verbatim.src)
-    .pipe(gulp.dest(config.paths.verbatim.dest));
+  return gulp.src(config.paths.verbatim.source)
+    .pipe(gulp.dest(config.paths.verbatim.destination));
 });
 
 gulp.task("browser-sync", function() {
-  browserSync({
+  return browserSync({
     server: {
-      baseDir: "./build"
+      baseDir: config.paths.html.destination
     }
   });
 });
 
-gulp.task("build", ["bower", "html", "scripts", "css", "sass", "images", "verbatim"]);
+gulp.task("build", ["bower", "html", "javascript", "css", "sass", "images", "verbatim"]);
 
 gulp.task("default", ["build", "browser-sync"], function(){
-  gulp.watch(config.paths.html.src, ["html", browserSync.reload]);
-  gulp.watch(config.paths.javascript.src, ["scripts", browserSync.reload]);
-  gulp.watch(config.paths.bower.src, ["bower", browserSync.reload]);
-  gulp.watch(config.paths.images.src, ["images", browserSync.reload]);
-  gulp.watch(config.paths.verbatim.src, ["verbatim", browserSync.reload]);
+  // Watch .html files
+  gulp.watch(config.paths.html.source, ["html", browserSync.reload]);
+  // Watch .js files
+  gulp.watch(config.paths.javascript.source, ["javascript", browserSync.reload]);
+  // Watch image files
+  gulp.watch(config.paths.images.source, ["images", browserSync.reload]);
+  // Watch for files we copy verbatim
+  gulp.watch(config.paths.verbatim.source, ["verbatim", browserSync.reload]);
 
-  gulp.watch(config.paths.css.src, ["css"]);
-  //gulp.watch(config.paths.less.src, ["less"]);
-  gulp.watch(config.paths.sass.src, ["sass"]);
+  // Watch .css files
+  gulp.watch(config.paths.css.source, ["css"]);
+  // Watch .less files
+  //gulp.watch(config.paths.less.source, ["less"]);
+  // Watch .scss files
+  gulp.watch(config.paths.sass.source, ["sass"]);
+
+  // Watch bower files
+  gulp.watch(config.paths.bower.source, ["bower", browserSync.reload]);
 });
